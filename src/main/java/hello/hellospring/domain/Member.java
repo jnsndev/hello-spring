c7eae0451ea59b2ce7750011fd5f724fe43a168a
jnsndev <jnsn.yang@gmail.com>
2021-02-20 20:48:26 +0900

    JPA로 변경

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
     // 고객이 입력한 id가 아니라 시스템이 정해준 id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
